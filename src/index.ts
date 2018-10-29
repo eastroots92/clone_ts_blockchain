57d3a90c087d7f8221a2fc6b98a78d6a53db2f75
Eastroots92 <eastroots92@gmail.com>
2018-10-30 01:56:22 +0900

    feat: create new block using createNewBlock method
    createNewBlock을 이용해 새 block을 만든다.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   static calculateBlockHash = (index: number,
     previousHash: string,
     timestamp: number,
@@ -14,6 +8,19 @@ class Block {
       previousHash + 
       timestamp + 
       data).toString();
+
+  static validateStructure = (aBlock: Block): boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data == "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
   constructor(index: number,
     hash: string,
@@ -55,10 +62,39 @@ const createNewBlock = (data: string): Block => {
     previousBlock.hash,
     data,
     newTimestamp);
-
+  addBlock(newBlock);
   return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash (aBlock.index, 
+  aBlock.previousHash,
+  aBlock.timestamp,
+  aBlock.data);
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
 }
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
